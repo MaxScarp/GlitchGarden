c5aad2de27bfee1413e1e264617381e3fffa72df
Massimiliano Scarpetta <massy1997.lavoro@gmail.com>
2021-04-06 15:51:17 +0200

    Creato Level Loader per gestire il tempo ed il caricamento tra Splash Screen e Start Menu Screen.

---
+++ Assets/Scripts/LevelLoader.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelLoader : MonoBehaviour
+{
+    [SerializeField] int secondsToWait = 3;
+
+    int currentSceneIndex;
+
+    private void Start()
+    {
+        currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        if(currentSceneIndex == 0)
+        {
+            StartCoroutine(WaitForSplashScreen());
+        }
+    }
+
+    IEnumerator WaitForSplashScreen()
+    {
+        yield return new WaitForSeconds(secondsToWait);
+        LoadNextScene();
+    }
+
+    private void LoadNextScene()
+    {
+        SceneManager.LoadScene(currentSceneIndex + 1);
+    }
+}
