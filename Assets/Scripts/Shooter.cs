906554fcd17860acf49fec3b2f7d8d56e7db1efe
Massimiliano Scarpetta <massy1997.lavoro@gmail.com>
2021-04-19 19:21:44 +0200

    Scritto codice in Shooter Class per vedere se un Attacker si trova sulla mia stessa linea. Cambio animazione in base al controllo descritto prima.

--- Assets/Scripts/Shooter.cs
+++ Assets/Scripts/Shooter.cs
@@ -5,6 +5,43 @@ public class Shooter : MonoBehaviour
     [SerializeField] GameObject gun;
     [SerializeField] Projectile projectilePrefab;
 
+    AttackerSpawner myLaneSpawner;
+    Animator animator;
+
+    private void Start()
+    {
+        animator = GetComponent<Animator>();
+        SetLaneSpawner();
+    }
+
+    private void Update()
+    {
+        if(IsAttackerInLane())
+        {
+            animator.SetBool("isAttacking", true);
+        }
+        else
+        {
+            animator.SetBool("isAttacking", false);
+        }
+    }
+
+    private void SetLaneSpawner()
+    {
+        AttackerSpawner[] spawners = FindObjectsOfType<AttackerSpawner>();
+
+        foreach(AttackerSpawner spawner in spawners)
+        {
+            bool isCloseEnough = (Mathf.Abs(spawner.transform.position.y - transform.position.y) <= Mathf.Epsilon);
+
+            if(isCloseEnough)
+            {
+                myLaneSpawner = spawner;
+            }
+        }
+    }
+
+    private bool IsAttackerInLane() => !(myLaneSpawner.transform.childCount <= 0);
     public void Fire()
     {
         Instantiate(projectilePrefab, gun.transform.position, transform.rotation);
